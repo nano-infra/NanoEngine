d3af99b6f9f613b47bb657b7ec85509af6c1b5a0
JimyMa <33408125+JimyMa@users.noreply.github.com>
2025-11-09 15:07:10 +0800

    Pd disagg bugfix (#5)
    * init_loop_count
    
    * bug fix and code refine
    
    * pd bugfix

--- nanodeploy/engine/llm_engine.py
+++ nanodeploy/engine/llm_engine.py
@@ -15,6 +15,10 @@ from nanodeploy.config import Config
 from nanodeploy.engine.ray_executor import RayExecutor
 from nanodeploy.engine.scheduler import Scheduler
 from nanodeploy.engine.sequence import Sequence
+from nanodeploy.logging import get_logger
+
+
+logger = get_logger()
 
 
 class LLMEngine:
@@ -77,6 +81,15 @@ class LLMEngine:
         post_sch_end = 0
         if is_prefill and self.config.mode == "decode":
             if not self.config.dummy_prefill:
+                logger.info("perform migration")
+                for seqs in dp_sp_tp_seqs:
+                    for seq in seqs:
+                        logger.info(
+                            f"{seq.block_ctx(seq.backup_engine_id).block_location, seq.block_ctx(seq.active_engine_id).block_location}"
+                        )
+                        seq.block_ctx(seq.active_engine_id).num_dispatched_tokens[
+                            seq.block_ctx().master_sp_rank
+                        ] += 1
                 self.executor.migrate(dp_sp_tp_seqs)
             else:
                 [[seq.append_token(0) for seq in seqs] for seqs in dp_seqs]
