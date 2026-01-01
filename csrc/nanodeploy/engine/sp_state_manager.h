ffb6a78c8ba46cae01ce385479b8a298a29bd6aa
nannaer <97857955+nannaer@users.noreply.github.com>
2026-01-01 15:43:00 +0800

    modify default enable sp segment size

--- csrc/nanodeploy/engine/sp_state_manager.h
+++ csrc/nanodeploy/engine/sp_state_manager.h
@@ -18,7 +18,7 @@ enum class RoutingStrategy {
 
 class SPStateManager {
 public:
-    static constexpr int segment_size = 1024;
+    static constexpr int segment_size = 65536;
 
     SPStateManager(const std::string& engine_id,
                    int                attention_sp,
