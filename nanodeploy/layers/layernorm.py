5877b487e9a8140eadfb34be24abc45e05a41c3e
nannaer <97857955+nannaer@users.noreply.github.com>
2026-01-16 12:30:19 +0800

    optimize layernorm

--- nanodeploy/layers/layernorm.py
+++ nanodeploy/layers/layernorm.py
@@ -1,6 +1,19 @@
 import torch
 from torch import nn
 
+try:
+    from nanodeploy.kernels.layernorm import (fused_add_rms_norm_triton,
+                                              rms_norm_triton)
+    HAS_TRITON = True
+except ImportError:
+    HAS_TRITON = False
+
+try:
+    from vllm import _custom_ops as ops
+    HAS_VLLM_OPS = True
+except ImportError:
+    HAS_VLLM_OPS = False
+
 
 class RMSNorm(nn.Module):
 
@@ -17,6 +30,13 @@ class RMSNorm(nn.Module):
         self,
         x: torch.Tensor,
     ) -> torch.Tensor:
+        if HAS_VLLM_OPS and x.is_cuda:
+            out = torch.empty_like(x)
+            ops.rms_norm(out, x, self.weight.data, self.eps)
+            return out
+        if HAS_TRITON and x.is_cuda:
+            return rms_norm_triton(x, self.weight, self.eps)
+
         orig_dtype = x.dtype
         x = x.float()
         var = x.pow(2).mean(dim=-1, keepdim=True)
@@ -29,6 +49,12 @@ class RMSNorm(nn.Module):
         x: torch.Tensor,
         residual: torch.Tensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
+        if HAS_VLLM_OPS and x.is_cuda and residual.is_cuda:
+            ops.fused_add_rms_norm(x, residual, self.weight.data, self.eps)
+            return x, residual
+        if HAS_TRITON and x.is_cuda and residual.is_cuda:
+            return fused_add_rms_norm_triton(x, residual, self.weight, self.eps)
+
         orig_dtype = x.dtype
         x = x.float().add_(residual.float())
         residual = x.to(orig_dtype)
